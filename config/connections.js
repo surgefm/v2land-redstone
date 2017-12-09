2095063e815e53c5370c7a6b66cc34a5e4370271
IncredibLink <incrediblelink@gmail.com>
2017-12-09 17:48:58 -0500

    Initialize Postgres connection configuration

--- config/connections.js
+++ config/connections.js
@@ -32,6 +32,14 @@ module.exports.connections = {
     adapter: 'sails-disk'
   },
 
+  postgresql: {
+    adapter: 'sails-postgresql',
+    host: process.env.POSTGRES_HOST || '127.0.0.1',
+    user: process.env.POSTGRES_USER || 'postgres',
+    password: process.env.POSTGRES_PWD,
+    database: process.env.POSTGRES_DB || 'v2land'
+  }
+
   /***************************************************************************
   *                                                                          *
   * MySQL is the world's most popular relational database.                   *
@@ -64,24 +72,6 @@ module.exports.connections = {
   //   password: 'password', //optional
   //   database: 'your_mongo_db_name_here' //optional
   // },
-
-  /***************************************************************************
-  *                                                                          *
-  * PostgreSQL is another officially supported relational database.          *
-  * http://en.wikipedia.org/wiki/PostgreSQL                                  *
-  *                                                                          *
-  * Run: npm install sails-postgresql                                        *
-  *                                                                          *
-  *                                                                          *
-  ***************************************************************************/
-  // somePostgresqlServer: {
-  //   adapter: 'sails-postgresql',
-  //   host: 'YOUR_POSTGRES_SERVER_HOSTNAME_OR_IP_ADDRESS',
-  //   user: 'YOUR_POSTGRES_USER', // optional
-  //   password: 'YOUR_POSTGRES_PASSWORD', // optional
-  //   database: 'YOUR_POSTGRES_DB' //optional
-  // }
-
 
   /***************************************************************************
   *                                                                          *
