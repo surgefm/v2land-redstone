d373f2699bf58411920c1d4ed8c09b1834583046
Vincent Chan <cdzos97@gmail.com>
2017-12-14 16:25:46 +0800

    add unit tests support

--- Makefile
+++ Makefile
@@ -1,8 +1,14 @@
 EXE_PATH=./node_modules/.bin/eslint
-API_PATH=api/**
+API_PATH=api/**.js
+TEST_PATH=tests/**.js
 
 eslint:
 	$(EXE_PATH) $(API_PATH)
+	$(EXE_PATH) $(TEST_PATH)
 
 eslint-fix:
 	$(EXE_PATH) $(API_PATH) --fix
+	$(EXE_PATH) $(TEST_PATH) --fix
+
+test:
+	node ./node_modules/mocha/bin/mocha tests/bootstrap.test.js tests/integration/**/*.test.js
---
+++ tests/bootstrap.test.js
@@ -0,0 +1,19 @@
+let sails = require('sails');
+
+before(function(done) {
+  // Increase the Mocha timeout so that Sails has enough time to lift.
+  this.timeout(5000);
+
+  sails.lift({
+    // configuration for testing purposes
+  }, function(err) {
+    if (err) return done(err);
+    // here you can load fixtures, etc.
+    done(err, sails);
+  });
+});
+
+after(function(done) {
+  // here you can clear fixtures, etc.
+  sails.lower(done);
+});
