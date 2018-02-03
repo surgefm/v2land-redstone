8cb48b33ecf6ddd52618adc4a450af94d2b20de4
Arash|Petry <jiaxi@umail.ucsb.edu>
2018-02-03 16:09:25 +0800

    isLoggedIn completegit add .

--- api/policies/isLoggedIn.js
+++ api/policies/isLoggedIn.js
@@ -8,18 +8,11 @@
  */
 
 module.exports = async function(req, res, next) {
-    // console.log('getClientById = ' + getClientById);
-    // console.log('req.session.clientId = ' + req.session.clientId);
-    // console.log('req.session.client = ' + req.session.client);
-    let client = await getClientById(req.session.clientId);
-    // console.log('client = ' + client);
+    let client = await getClient(req.session.clientId);
     if (client) {
-        // console.log('next');
         req.session.client = client;
         return next();
     } else {
-        // actions if not logged in
-        // console.log('forbidden');
         return res.status(401).json({
             message: '你还未登录哦',
         });
@@ -27,15 +20,14 @@ module.exports = async function(req, res, next) {
 };
 
 /**
- * getClientById
+ * getClient
  *
  * @module      :: Policy
  * @description :: Check if the clientId is undefined or invalid; if valid, return a client instance
  * @returns     :: a client instance(if client's found) or false (if not found)
  */
-async function getClientById(clientId) {
+async function getClient(clientId) {
     if (!clientId) {
-        // console.log('clientId == false');
         return false;
     }
     let client = await Client.findOne({ id: clientId });
@@ -43,6 +35,5 @@ async function getClientById(clientId) {
         delete req.session.clientId;
         return false;
     }
-    // console.log('clientId == true');
     return client;
 }
