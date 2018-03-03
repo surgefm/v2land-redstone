6bb27df66ae6679609a24b4141fa8d2e3f83cff1
IncredibLink <simon.zehua.li@gmail.com>
2018-03-03 02:53:24 -0500

    在获取用户社交绑定时不返回 accessToken

--- api/services/ClientService.js
+++ api/services/ClientService.js
@@ -14,6 +14,7 @@ module.exports = {
       })
       .populate('auths', {
         where: { profileId: { '>=': 1 } },
+        select: ['id', 'site', 'profileId', 'profile'],
       })
       .populate('events', {
         sort: 'updatedAt DESC',
