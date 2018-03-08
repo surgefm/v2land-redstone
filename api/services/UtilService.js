b8df022071caf348fcb2c60c02b6cdb4c734f38f
IncredibLink <simon.zehua.li@gmail.com>
2018-03-08 00:41:22 -0500

    文件后缀名改用小写

--- api/services/UtilService.js
+++ api/services/UtilService.js
@@ -5,7 +5,7 @@ module.exports = {
   generateFilename: (file) => {
     const { filename } = file;
     const parts = filename.split('.');
-    const extension = parts[parts.length - 1];
+    const extension = parts[parts.length - 1].toLowerCase();
     const newFilename = md5(Date.now() + Math.random() * 9999 + filename) + '.' + extension;
     return newFilename;
   },
