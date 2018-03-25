e9a7b3656b4547cf23f19237a4c1699e258d6477
IncredibLink <simon.zehua.li@gmail.com>
2018-03-25 01:47:35 -0400

    更新默认配置

--- config/globals.js
+++ config/globals.js
@@ -10,8 +10,8 @@
  */
 module.exports.globals = {
 
-  site: process.env.SITE || 'https://langchao.co',
-  api: process.env.API || 'https://a.langchao.co',
+  site: process.env.SITE || 'https://langchao.org',
+  api: process.env.API || 'https://api.langchao.org',
   notification: process.env.ENABLE_NOTIFICATION,
   officialAccount: {
     twitter: process.env.OFFICIAL_TWITTER || '768458621613072384',
