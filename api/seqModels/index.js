aa0f7fc9c43678b17b4125556d334f5cec5a2aae
Vincent Chan <cdzos97@gmail.com>
2018-08-01 18:24:19 +0800

    add relationships

---
+++ api/seqModels/index.js
@@ -0,0 +1,38 @@
+const Auth = require('./Auth.js`');
+const Client = require('./Client.js');
+const Critique = require('./Critique.js');
+const Event = require('./Event.js');
+const HeaderImage = require('./HeaderImage.js');
+const News = require('./News.js');
+const Notification = require('./Notification.js');
+const Record = require('./Record.js');
+const Stack = require('./Stack.js');
+const Subscription = require('./Subscription.js');
+
+Event.hasOne(HeaderImage, {
+  as: 'headerImage',
+  foreignKey: 'headerImage',
+});
+
+Event.hasMany(Stack, {
+  foreignKey: 'event',
+  sourceKey: 'id',
+});
+
+Stack.hasMany(News, {
+  foreignKey: 'stack',
+  sourceKey: 'id',
+});
+
+module.exports = {
+  Auth,
+  Client,
+  Critique,
+  Event,
+  HeaderImage,
+  News,
+  Notification,
+  Record,
+  Stack,
+  Subscription,
+};
