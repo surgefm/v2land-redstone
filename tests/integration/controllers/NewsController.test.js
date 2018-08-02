6b988a418d6b2d50510e207564e3ce58d85fe65b
Vincent Chan <cdzos97@gmail.com>
2018-08-02 23:56:52 +0800

    remove junk tests

--- tests/integration/controllers/NewsController.test.js
+++ tests/integration/controllers/NewsController.test.js
@@ -1,10 +1,10 @@
 const request = require('supertest');
 const urlencode = require('urlencode');
-const assert = require('assert');
+// const assert = require('assert');
 const SeqModels = require('../../../seqModels');
 
 let agent;
-let newsId;
+// let newsId;
 
 const testEmail = process.env.TEST_EMAIL?
   process.env.TEST_EMAIL : 'vincent@langchao.co';
@@ -62,68 +62,5 @@ describe.skip('NewsController', function() {
         time: new Date(),
       })
       .expect(404);
-  });
-
-  it('should create news', async function() {
-    await agent
-      .post(`/event/${urlencode(testEventName)}/news`)
-      .send({
-        url: 'https://langchao.co',
-        source: 'source',
-        title: '浪潮今天不上线',
-        abstract: '浪潮今天不上线',
-        time: new Date(),
-      })
-      .expect(201);
-  });
-
-  it('should create news', async function() {
-    const res = await agent
-      .post(`/event/${urlencode(testEventName)}/news`)
-      .send({
-        url: 'https://langchao.co/',
-        source: 'source',
-        title: '浪潮今天不上线啦',
-        abstract: '浪潮今天不上线啦',
-        time: new Date(),
-      })
-      .expect(201);
-    newsId = res.body.news.id;
-  });
-
-  it('should return all pending news', async function() {
-    const res = await agent
-      .get('/news/pending')
-      .expect(200);
-    assert.equal(res.body.newsCollection.length, 2);
-  });
-
-  it('should change title success', async function() {
-    const res = await agent
-      .put(`/news/${newsId}`)
-      .send({
-        title: '浪潮今天不上线啦啦啦',
-      })
-      .expect(201);
-    assert.equal('浪潮今天不上线啦啦啦', res.body.news.title);
-  });
-
-
-  it('should change to rejected success', async function() {
-    await agent
-      .put(`/news/${newsId}`)
-      .send({
-        status: 'rejected',
-      })
-      .expect(201);
-  });
-
-  it('should change to admitted success', async function() {
-    await agent
-      .put(`/news/${newsId}`)
-      .send({
-        status: 'admitted',
-      })
-      .expect(201);
   });
 });
