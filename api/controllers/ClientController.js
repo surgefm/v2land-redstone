556b6a6ce9e5d0dc06f0e7fe8d4eae1793f2d78f
Vincent Chan <cdzos97@gmail.com>
2018-08-02 18:43:22 +0800

    rewrite changePassword

--- api/controllers/ClientController.js
+++ api/controllers/ClientController.js
@@ -160,55 +160,65 @@ module.exports = {
 
     const selfClient = req.currentClient;
 
-    const targetClient = await Client.findOne({
-      id: targetId,
-    });
-
-    if (typeof targetClient === 'undefined') {
-      return res.status(500).json({
-        message: '找不到目标用户',
-      });
-    }
-
-    if (targetId !== clientId && selfClient.role !== 'admin') {
-      return res.status(500).json({
-        message: '您没有修改此用户密码的权限',
-      });
-    }
-
     try {
-      salt = await bcrypt.genSalt(10);
-    } catch (err) {
-      sails.log.error(err);
-      return res.status(500).json({
-        message: '服务器发生未知错误，请联系开发者',
-      });
-    }
+      await sequelize.transaction(async transaction => {
+        const targetClient = await SeqModels.Client.findOne({
+          where: {
+            id: targetId,
+          },
+          transaction,
+        });
 
-    try {
-      hash = await bcrypt.hash(data.password, salt);
-    } catch (err) {
-      sails.log.error(err);
-      return res.status(500).json({
-        message: '服务器发生未知错误，请联系开发者',
-      });
-    }
+        if (typeof targetClient === 'undefined') {
+          return res.status(500).json({
+            message: '找不到目标用户',
+          });
+        }
 
-    try {
-      await SQLService.update({
-        where: { id: targetId },
-        model: 'client',
-        data: {
-          password: hash,
-        },
-        client: targetId,
-        action: 'updateClientPassword',
-      });
+        if (targetId !== clientId && selfClient.role !== 'admin') {
+          return res.status(500).json({
+            message: '您没有修改此用户密码的权限',
+          });
+        }
 
-      res.send(201, {
-        message: '更新密码成功',
+        try {
+          salt = await bcrypt.genSalt(10);
+        } catch (err) {
+          sails.log.error(err);
+          return res.status(500).json({
+            message: '服务器发生未知错误，请联系开发者',
+          });
+        }
+
+        try {
+          hash = await bcrypt.hash(data.password, salt);
+        } catch (err) {
+          sails.log.error(err);
+          return res.status(500).json({
+            message: '服务器发生未知错误，请联系开发者',
+          });
+        }
+
+        targetClient.password = hash;
+        await targetClient.save({ transaction });
+
+        await SeqModels.Record.create({
+          operation: 'update',
+          model: 'client',
+          data: {
+            password: hash,
+          },
+          action: 'updateClientPassword',
+          client: targetId,
+          target: targetId,
+        }, { transaction });
+
+        res.send(201, {
+          message: '更新密码成功',
+        });
       });
     } catch (err) {
+      console.log(err);
       return res.serverError(err);
     }
   },
