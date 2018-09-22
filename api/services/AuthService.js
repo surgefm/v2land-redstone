4933822c2da48864b260da5a1af266d2ca9400fa
IncredibLink <incrediblelink@gmail.com>
2018-09-22 04:33:43 -0400

    增加 EventService.findEvent 的 eventOnly 选项，可以只要事件信息
    获取网站名
    
    我们是微软删库部
    
    令 SeqModels export Contact
    
    移除 sequelize cli
    
    这一份情，难舍难了
    
    Update yarn.lock

--- api/services/AuthService.js
+++ api/services/AuthService.js
@@ -9,4 +9,15 @@ module.exports = {
     return auth;
   },
 
+  getSiteName(method) {
+    switch (method) {
+    case 'twitterAt':
+      return 'twitter';
+    case 'weiboAt':
+      return 'weibo';
+    default:
+      return method;
+    }
+  },
+
 };
