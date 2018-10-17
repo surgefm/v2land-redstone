765b46ac4f7ba06bb5154c0fc1ae39ea60c4fc03
IncredibLink <incrediblelink@gmail.com>
2018-10-17 00:55:05 -0400

    Rewrite SubscriptionController

--- seqModels/Notification.js
+++ seqModels/Notification.js
@@ -21,7 +21,7 @@ const Notification = global.sequelize.define('notification', {
   },
   status: {
     type: Sequelize.ENUM([
-      'pending', 'ongoing', 'complete', 'invalid',
+      'pending', 'ongoing', 'complete',
     ]),
     defaultValue: 'pending',
   },
