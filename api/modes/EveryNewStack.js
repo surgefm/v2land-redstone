bfc29d2db6ffe7ca5a1ea2d9feaa5c825c0c45c0
IncredibLink <incrediblelink@gmail.com>
2018-10-18 00:56:37 -0400

    Implement more modes

--- api/modes/EveryNewStack.js
+++ api/modes/EveryNewStack.js
@@ -1,3 +1,8 @@
+const SeqModels = require('../../seqModels');
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
+const { MissingParameterError } = require('../../utils/errors');
+
 module.exports = {
   needStack: true,
   new: async () => {
@@ -6,11 +11,35 @@ module.exports = {
   notified: async () => {
     return new Date('1/1/3000');
   },
-  getTemplate: async ({ event, stack }) => {
+  getTemplate: async ({ notification, event, stack }) => {
+    if (!event) {
+      if (!notification) {
+        throw new MissingParameterError('notification');
+      }
+      event = notification.event;
+    }
+
+    if (typeof event === 'number') {
+      event = await SeqModels.Event.findOne({
+        where: { id: event },
+      });
+    }
+
+    if (!stack) {
+      stack = await SeqModels.Stack.findOne({
+        where: {
+          event: event.id,
+          status: 'admitted',
+          order: { [Op.gte]: 0 },
+        },
+        order: [['order', ASC]],
+      });
+    }
+
     return {
       subject: `${event.name} 有了新的进展`,
       message: `${event.name} 最新进展：「${stack.title}」`,
-      button: '点击按钮查看新闻',
+      button: '点击按钮查看进展',
       url: `${sails.config.globals.site}/${event.id}?stack=${stack.id}`,
     };
   },
