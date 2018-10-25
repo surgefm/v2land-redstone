733bc91d98e9842edac0de40773ca6f93dfb3dbc
IncredibLink <incrediblelink@gmail.com>
2018-10-25 13:50:41 -0400

    使用 Instant/Report Notification 方法

--- api/hooks/notificationInit/index.js
+++ api/hooks/notificationInit/index.js
@@ -1,11 +1,13 @@
-const checkNotification = require('../../../notifications/check');
+const checkInstantNotification = require('../../../notifications/checkInstantNotification');
+const checkReportNotification = require('../../../notifications/checkReportNotification');
 
 module.exports = function NotificationInit(sails) {
   return {
     initialize: function(cb) {
       sails.on('hook:orm:loaded', () => {
         if (sails.config.globals.notification) {
-          setTimeout(checkNotification, checkInterval);
+          setTimeout(checkInstantNotification, 500);
+          setTimeout(checkReportNotification, 500);
         }
 
         cb();
