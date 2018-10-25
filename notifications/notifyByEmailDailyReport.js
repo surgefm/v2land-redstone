92cae715b12c4df2abf00d4ceabaaaba2a481c41
IncredibLink <incrediblelink@gmail.com>
2018-10-25 14:57:52 -0400

    将提醒信息添加至汇总报告中

---
+++ notifications/notifyByEmailDailyReport.js
@@ -0,0 +1,31 @@
+/**
+ * 将通知添加到每日邮件简讯中
+ */
+const SeqModels = require('../seqModels');
+
+async function notifyByEmailDailyReport(subscription, template, notification) {
+  const time = ModeService.daily.new();
+
+  await sequelize.transaction(async transaction => {
+    const report = await SeqModels.Report.findOrCreate({
+      where: {
+        time,
+        status: 'pending',
+        type: 'daily',
+        owner: subscription.subscriber,
+      },
+      transaction,
+    })[0];
+
+    await SeqModels.ReportNotification.findOrCreate({
+      where: {
+        status: 'pending',
+        reportId: report.id,
+        notificationId: notification.id,
+      },
+      transaction,
+    });
+  });
+}
+
+module.exports = notifyByEmailDailyReport;
