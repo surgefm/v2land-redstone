7947d71f3177fa0d708c3aba4f73d47e02d85028
IncredibLink <simon.zehua.li@gmail.com>
2018-11-03 19:28:14 -0400

    移除冗余代码

--- api/services/NotificationService.js
+++ api/services/NotificationService.js
@@ -1,4 +1,3 @@
-const SeqModels = require('../../seqModels');
 const notifyWhenEventCreated = require('./NotificationService/notifyWhenEventCreated');
 const notifyWhenNewsCreated = require('./NotificationService/notifyWhenNewsCreated');
 const notifyWhenEventStatusChanged = require('./NotificationService/notifyWhenEventStatusChanged');
@@ -15,88 +14,6 @@ const NotificationService = {
 
   notified: async (mode, event) => {
     return ModeService[mode].notified({ event });
-  },
-
-  updateForNewNews: async (event, news, force = false) => {
-    const latestNews = await News.findOne({
-      where: { event: event.id, status: 'admitted' },
-      sort: 'time DESC',
-    });
-
-    if (!force && (!latestNews || (+latestNews.id !== +news.id))) return;
-
-    const record = await Record.count({
-      model: 'News',
-      target: news.id,
-      action: 'notifyNewNews',
-    });
-
-    if (record) return;
-
-    const notificationCollection = await Notification.find({
-      event: event.id,
-      mode: ['new', '7DaysSinceLatestNews'],
-    });
-
-    for (const notification of notificationCollection) {
-      const mode = ModeService[notification.mode];
-      const time = await mode.update({ notification, event, latestNews });
-      notification.time = time;
-      notification.status = 'active';
-      await notification.save();
-    }
-
-    await Record.create({
-      model: 'News',
-      target: news.id,
-      operation: 'create',
-      action: 'notifyNewNews',
-    });
-
-    return;
-  },
-
-  updateForNewStack: async (event, stack, force = false) => {
-    const modeSet = ['EveryNewStack', '30DaysSinceLatestStack'];
-
-    const latestStack = await SeqModels.Stack.findOne({
-      where: {
-        event: event.id,
-        status: 'admitted',
-        order: 1,
-      },
-      sort: 'order DESC',
-    });
-
-    if (!force && (!latestStack || (+latestStack.id !== +stack.id))) return;
-
-    const record = await SeqModels.Record.count({
-      model: 'Stack',
-      target: stack.id,
-      action: 'notifyNewStack',
-    });
-
-    if (record) return;
-
-    await sequelize.transaction(async transaction => {
-      for (const mode of modeSet) {
-        await SeqModels.Notification.create({
-          time: await ModeService[mode].new({ event, stack, transaction }),
-          status: 'pending',
-          mode,
-          content: { event, stack },
-        }, { transaction });
-      }
-
-      await SeqModels.Record.create({
-        model: 'Stack',
-        target: stack.id,
-        operation: 'create',
-        action: 'notifyNewStack',
-      }, { transaction });
-    });
-
-    return;
   },
 
 };
