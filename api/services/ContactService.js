108ece3391ad7c63d75490a46a10bf2139de32c0
IncredibLink <simon.zehua.li@gmail.com>
2018-11-07 01:21:20 -0500

    SubscriptionController 支持 emailDailyReport

--- api/services/ContactService.js
+++ api/services/ContactService.js
@@ -2,6 +2,8 @@ module.exports = {
 
   getTypeFromMethod: (method) => {
     switch (method) {
+    case 'emailDailyReport':
+      return 'email';
     case 'twitterAt':
       return 'twitter';
     case 'weiboAt':
