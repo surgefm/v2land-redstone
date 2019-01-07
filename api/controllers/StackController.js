b123ab0bca02370c57e3d53c16961a9fc3f7ef6f
IncredibLink <link@langchao.org>
2019-01-07 22:35:38 +0800

    Update StackController.js

--- api/controllers/StackController.js
+++ api/controllers/StackController.js
@@ -108,7 +108,9 @@ const StackController = {
         return res.status(cb.status).json(cb.message);
       });
     } catch (err) {
-      console.error(err);
+      if (err.message === '一个进展必须在含有一个已过审新闻的情况下方可开放') {
+        return res.status(400).json({ message: err.message });
+      }
       return res.serverError(err);
     }
   },
