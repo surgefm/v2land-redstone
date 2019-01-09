3ba0c2c79e1451a0cfb6409fdbb62fe25506762b
Vincent Chan <vincent@langchao.org>
2019-01-09 15:24:52 +0800

    fix format for Slack

--- api/services/NotificationService/notifyWhenEventCreated.js
+++ api/services/NotificationService/notifyWhenEventCreated.js
@@ -2,7 +2,7 @@ async function notifyWhenEventCreated(event, handler) {
   const client = await ClientService.findClient(handler);
   const username = (client && client.username) || '游客';
   const isAdmin = await UtilService.isAdmin(client);
-  let content = `*${username}* 提交了事件「*${event.name}*」` +
+  let content = `*${username}* 提交了事件 *${event.name}* ` +
   `，请管理员尽快<${sails.config.globals.site}/admin/event|审核>`;
 
   if (isAdmin && event.status === 'admitted') {
