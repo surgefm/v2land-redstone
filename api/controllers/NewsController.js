64685aa3208e393b772f25c35c6227981f4a5721
IncredibLink <link@langchao.org>
2019-01-17 12:30:12 -0500

    Update NewsController.js

--- api/controllers/NewsController.js
+++ api/controllers/NewsController.js
@@ -145,7 +145,10 @@ module.exports = {
     }
 
     const changes = {};
-    for (const i of ['url', 'source', 'title', 'abstract', 'time', 'status', 'comment', 'stackId']) {
+    for (const i of [
+      'url', 'source', 'title', 'abstract', 'time',
+      'status', 'comment', 'stackId', 'isInTemporaryStack',
+    ]) {
       if (data[i] && data[i] !== news[i]) {
         changes[i] = data[i];
       }
