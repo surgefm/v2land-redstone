ca86d397cfe7b95e7b72a39f188c62dd3c023723
IncredibLink <link@langchao.org>
2019-01-18 15:14:27 -0500

    Testflight

--- api/controllers/EventController.js
+++ api/controllers/EventController.js
@@ -12,21 +12,7 @@ const urlTrimmer = require('v2land-url-trimmer');
 
 const EventController = {
 
-  getEvent: async (req, res) => {
-    const name = req.param('eventName');
-    const event = await EventService.findEvent(name, {
-      includes: req.query,
-    });
-
-    if (event) {
-      event.contribution = await EventService.getContribution(event, true);
-      res.status(200).json(event);
-    } else {
-      res.status(404).json({
-        message: '未找到该事件',
-      });
-    }
-  },
+  getEvent: require('./EventController/getEvent'),
 
   getAllPendingEvents: async (req, res) => {
     const eventCollection = await SeqModels.Event.findAll({
