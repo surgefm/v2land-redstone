4f0a3c7c66ce508c1d30987e2349117e36c27114
IncredibLink <link@langchao.org>
2019-01-18 16:17:29 -0500

    Refractor ClientController

---
+++ api/controllers/ClientController/register.js
@@ -0,0 +1,94 @@
+const bcrypt = require('bcryptjs');
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
+const SeqModels = require('../../../seqModels');
+
+async function register (req, res) {
+  const data = req.body;
+  let salt;
+  let hash;
+
+  try {
+    await sequelize.transaction(async transaction => {
+      let client = await SeqModels.Client.findOne({
+        where: {
+          [Op.or]: [
+            { username: data.username },
+            { email: data.email },
+          ],
+        },
+        transaction,
+      });
+
+      if (client) {
+        const message = client.username === data.username
+          ? '该用户名已被占用'
+          : '该邮箱已被占用';
+        return res.status(406).json({ message });
+      }
+
+      try {
+        salt = await bcrypt.genSalt(10);
+      } catch (err) {
+        return res.status(500).json({
+          message: 'Error occurs when generating salt',
+        });
+      }
+
+      try {
+        hash = await bcrypt.hash(data.password, salt);
+      } catch (err) {
+        return res.status(500).json({
+          message: 'Error occurs when generating hash',
+        });
+      }
+
+      client = await SeqModels.Client.create({
+        username: data.username,
+        password: hash,
+        email: data.email,
+        role: 'contributor',
+      }, {
+        raw: true,
+        transaction,
+      });
+
+      const verificationToken = ClientService.tokenGenerator();
+
+      await SeqModels.Record.create({
+        model: 'Client',
+        operation: 'create',
+        data: client,
+        target: client.id,
+        action: 'createClient',
+        client: req.session.clientId,
+      }, { transaction });
+
+      await SeqModels.Record.create({
+        model: 'Miscellaneous',
+        operation: 'create',
+        data: {
+          clientId: client.id,
+          verificationToken,
+          expire: new Date(Date.now() + 1000 * 60 * 60 * 24 * 3),
+        },
+        target: client.id,
+        action: 'createClientVerificationToken',
+        client: req.session.clientId,
+      }, { transaction });
+
+      req.session.clientId = client.id;
+      res.status(201).json({
+        message: '注册成功，请在三天内至邮箱查收验证邮件',
+        client,
+      });
+
+      EmailService.register(client, verificationToken);
+    });
+  } catch (err) {
+    console.log(err);
+    return res.serverError(err);
+  }
+}
+
+module.exports = register;
---
+++ api/controllers/ClientController/verifyToken.js
@@ -0,0 +1,60 @@
+const SeqModels = require('../../../seqModels');
+
+async function verifyToken (req, res) {
+  let token;
+  let id;
+  if (req.body && req.body.token && req.body.id) {
+    token = req.body.token;
+    id = req.body.id;
+  } else if (req.query && req.query.token && req.query.id) {
+    token = req.query.token;
+    id = req.query.id;
+  }
+
+  if (!(token && id)) {
+    return res.status(400).json({
+      message: '缺少参数：token 或 id',
+    });
+  }
+
+  let record = await SeqModels.Record.findOne({
+    where: {
+      action: 'createClientVerificationToken',
+      data: {
+        verificationToken: token,
+        clientId: id,
+      },
+    },
+  });
+
+  if (!record || !record[0]) {
+    return res.status(404).json({
+      message: '未找到该 token',
+    });
+  }
+
+  record = record[0];
+
+  if (new Date(record.data.expire).getTime() < Date.now()) {
+    return res.status(404).json({
+      message: '该 token 已失效',
+    });
+  }
+
+  const client = await SeqModels.Client.findById(record.data.clientId);
+
+  if (!client) {
+    return res.status(404).json({
+      message: '该 token 无效',
+    });
+  }
+
+  client.emailVerified = true;
+  await client.save();
+
+  res.status(201).json({
+    message: '账户验证成功',
+  });
+}
+
+module.exports = verifyToken;
