df629308416e02dbdd0cd17a27b6999fed9d14d5
IncredibLink <link@langchao.org>
2019-09-23 00:52:20 -0400

    Initialize OAuth2 framework

---
+++ api/controllers/OAuthController.js
@@ -0,0 +1,10 @@
+// const oauth2orize = require('oauth2orize');
+// const server = oauth2orize.createServer();
+
+module.exports = {
+
+  grant: async function (req, res) {},
+
+  exchange: async function (req, res) {},
+
+};
