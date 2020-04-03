0541ebed8b2faed91e36022be5f6454c19186bb9
IncredibLink <link@langchao.org>
2020-04-03 02:02:39 -0400

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,35 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const gulp = require('gulp');
+const { series } = gulp;
+const server = require('gulp-develop-server');
 const ts = require('gulp-typescript');
 const sourcemaps = require('gulp-sourcemaps');
 
-const tsProject = ts.createProject('tsconfig.json');
+const tsProject = ts.createProject('tsconfig.json', {
+  isolatedModules: true,
+});
 
-gulp.task('scripts', function() {
-  return tsProject.src()
+gulp.task('build', function(cb) {
+  tsProject.src()
     .pipe(sourcemaps.init())
     .pipe(tsProject())
     .pipe(sourcemaps.write('.', {
       includeContent: true,
       sourceRoot: '../src',
     }))
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('dist'))
+    .on('end', cb);
 });
+
+gulp.task('server', series('build', (cb) => {
+  server.listen({ path: 'dist/app.js' }, cb);
+}));
+
+gulp.task('watch', series('server', (cb) => {
+  gulp
+    .watch(['**/*.ts', '!node_modules/**/*'], { delay: 200 })
+    .on('change', series('build', server.restart))
+    .on('end', cb);
+}));
+
+gulp.task('default', series('server', 'watch'));
