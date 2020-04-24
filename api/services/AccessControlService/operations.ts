7101fa960e5ef8808ecb5b0c98fba56202b4cc3c
IncredibLink <link@langchao.org>
2020-04-23 23:22:29 -0400

    Add type definitions to AccessControlService.operations for development convenience

--- api/services/AccessControlService/operations.ts
+++ api/services/AccessControlService/operations.ts
@@ -20,15 +20,15 @@ export async function roleUsers(role: string) {
   });
 }
 
-export const addRoleParents = acl.addRoleParents.bind(acl);
-export const addUserRoles = acl.addUserRoles.bind(acl);
-export const allow = acl.allow.bind(acl);
-export const hasRole = acl.hasRole.bind(acl);
-export const isAllowed = acl.isAllowed.bind(acl);
-export const removeAllow = acl.removeAllow.bind(acl);
-export const removePermissions = acl.removePermissions.bind(acl);
-export const removeResource = acl.removeResource.bind(acl);
-export const removeRole = acl.removeRole.bind(acl);
-export const removeUserRoles = acl.removeUserRoles.bind(acl);
-export const userRoles = acl.userRoles.bind(acl);
-export const whatResources = acl.whatResources.bind(acl);
+export const addRoleParents: typeof acl.addRoleParents = acl.addRoleParents.bind(acl);
+export const addUserRoles: typeof acl.addUserRoles = acl.addUserRoles.bind(acl);
+export const allow: typeof acl.allow = acl.allow.bind(acl);
+export const hasRole: typeof acl.hasRole = acl.hasRole.bind(acl);
+export const isAllowed: typeof acl.isAllowed = acl.isAllowed.bind(acl);
+export const removeAllow: typeof acl.removeAllow = acl.removeAllow.bind(acl);
+export const removePermissions: typeof acl.removePermissions = acl.removePermissions.bind(acl);
+export const removeResource: typeof acl.removeResource = acl.removeResource.bind(acl);
+export const removeRole: typeof acl.removeRole = acl.removeRole.bind(acl);
+export const removeUserRoles: typeof acl.removeUserRoles = acl.removeUserRoles.bind(acl);
+export const userRoles: typeof acl.userRoles = acl.userRoles.bind(acl);
+export const whatResources: typeof acl.whatResources = acl.whatResources.bind(acl);
