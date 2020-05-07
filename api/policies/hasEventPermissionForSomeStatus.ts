b128e6aaafc0ad68762763baf2569822125f599f
makersmelx <makersmelx@gmail.com>
2020-05-08 00:39:38 +0800

    Change according to @simonzli request

--- api/policies/hasEventPermissionForSomeStatus.ts
+++ api/policies/hasEventPermissionForSomeStatus.ts
@@ -2,20 +2,26 @@ import { RedstoneRequest, RedstoneResponse, NextFunction } from '@Types';
 import { AccessControlService, EventService } from '@Services';
 import _ from 'lodash';
 
-const hasEventPermissionForSomeStatus = (action?: string, errorMessage?: string) => async (req: RedstoneRequest, res: RedstoneResponse, next: NextFunction) => {
+const hasEventPermissionForSomeStatus = (errorMessage?: string) => async (req: RedstoneRequest, res: RedstoneResponse, next: NextFunction) => {
   const name = req.params.eventName;
   const event = await EventService.findEvent(name);
   const eventId = await EventService.getEventId(name);
-  const resource = AccessControlService.getEventResourceId(eventId);
   let haveAccess = false;
   if (_.isUndefined(event)) {
+    return res.status(404).json({
+      message: '用户请求的事件不存在',
+    });
+  }
+  if (event.status === 'admitted') {
     haveAccess = true;
-  } else if (event.status === 'admitted') {
-    haveAccess = true;
-  } else if (event.status === 'hidden' || event.status === 'hidden') {
-    haveAccess = await AccessControlService.isAllowed(req.session.clientId, resource, 'view') || await AccessControlService.hasRole(req.session.clientId, 'editors');
+  } else if (event.status === 'hidden' || event.status === 'removed') {
+    haveAccess = await AccessControlService.hasRole(req.session.clientId, AccessControlService.getEventViewRolePlain(eventId));
+    haveAccess = haveAccess || await AccessControlService.hasRole(req.session.clientId, 'editors');
+    haveAccess = haveAccess || await AccessControlService.hasRole(req.session.clientId, 'admins');
   } else if (event.status === 'rejected') {
-    haveAccess = await AccessControlService.isAllowed(req.session.clientId, resource, 'owner') || await AccessControlService.hasRole(req.session.clientId, 'editors');
+    haveAccess = await AccessControlService.hasRole(req.session.clientId, AccessControlService.getEventOwnerRolePlain(eventId));
+    haveAccess = haveAccess || await AccessControlService.hasRole(req.session.clientId, 'editors');
+    haveAccess = haveAccess || await AccessControlService.hasRole(req.session.clientId, 'admins');
   }
   if (haveAccess) return next();
 
--- api/policies/index.ts
+++ api/policies/index.ts
@@ -2,7 +2,6 @@ import forbiddenRoute from './forbiddenRoute';
 import hasPermission from './hasPermission';
 import hasEventPermission from './hasEventPermission';
 import hasEventPermissionForSomeStatus from './hasEventPermissionForSomeStatus';
-import hasRole from './hasRole';
 import isAdmin from './isAdmin';
 import isLoggedIn from './isLoggedIn';
 import isManager from './isManager';
@@ -10,7 +9,7 @@ import sessionAuth from './sessionAuth';
 import uploadFile from './uploadFile';
 
 export type PolicyMiddleware = typeof forbiddenRoute | typeof hasPermission | typeof hasEventPermission |
-  typeof hasRole | typeof isAdmin | typeof isLoggedIn | typeof isManager |
+  typeof isAdmin | typeof isLoggedIn | typeof isManager |
   typeof sessionAuth | typeof uploadFile;
 
 export {
@@ -18,7 +17,6 @@ export {
   hasPermission,
   hasEventPermission,
   hasEventPermissionForSomeStatus,
-  hasRole,
   isAdmin,
   isLoggedIn,
   isManager,
@@ -31,7 +29,6 @@ export default {
   hasPermission,
   hasEventPermission,
   hasEventPermissionForSomeStatus,
-  hasRole,
   isAdmin,
   isLoggedIn,
   isManager,
