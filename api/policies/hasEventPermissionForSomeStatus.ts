ac6f4c15ce6369d219a0f4942b8182476ed58f52
makersmelx <makersmelx@gmail.com>
2020-05-09 00:10:29 +0800

    Implemnt access control in EventController.getEventList
    1. implementation
    2. EventController.getEvent can return event with admitted status if client is not logged in.

--- api/policies/hasEventPermissionForSomeStatus.ts
+++ api/policies/hasEventPermissionForSomeStatus.ts
@@ -14,19 +14,24 @@ const hasEventPermissionForSomeStatus = (errorMessage?: string) => async (req: R
   }
   if (event.status === 'admitted') {
     haveAccess = true;
-  } else if (event.status === 'hidden' || event.status === 'removed') {
-    haveAccess = await AccessControlService.hasRole(req.session.clientId, AccessControlService.getEventViewRolePlain(eventId));
-    haveAccess = haveAccess || await AccessControlService.hasRole(req.session.clientId, 'editors');
-    haveAccess = haveAccess || await AccessControlService.hasRole(req.session.clientId, 'admins');
-  } else if (event.status === 'rejected') {
-    haveAccess = await AccessControlService.hasRole(req.session.clientId, AccessControlService.getEventOwnerRolePlain(eventId));
-    haveAccess = haveAccess || await AccessControlService.hasRole(req.session.clientId, 'editors');
-    haveAccess = haveAccess || await AccessControlService.hasRole(req.session.clientId, 'admins');
+  } else {
+    if (typeof req.session === 'undefined') {
+      return res.status(401).json({
+        message: '请在登录后进行该操作',
+      });
+    }
+    if (event.status === 'hidden' || event.status === 'removed') {
+      haveAccess = await AccessControlService.hasRole(req.session.clientId, AccessControlService.getEventViewRolePlain(eventId));
+    } else if (event.status === 'rejected') {
+      haveAccess = await AccessControlService.hasRole(req.session.clientId, AccessControlService.getEventOwnerRolePlain(eventId));
+    }
+    haveAccess = haveAccess || await AccessControlService.hasRole(req.session.clientId, AccessControlService.roles.editors);
+    haveAccess = haveAccess || await AccessControlService.hasRole(req.session.clientId, AccessControlService.roles.admins);
   }
   if (haveAccess) return next();
 
   return res.status(403).json({
-    message: errorMessage || '用户没有该权限',
+    message: errorMessage || '用户没有查看事件的权限',
   });
 };
 
