4c408c83290229e1f8aef32a7ff6d99efef112c5
Zehua Li <simon.zehua.li@gmail.com>
2022-05-13 14:14:38 -0700

    Fix the bug where validation isn't checked when randomly generating usernames

--- api/services/ClientService/randomlyGenerateUsername.ts
+++ api/services/ClientService/randomlyGenerateUsername.ts
@@ -4,6 +4,13 @@ import generateRandomAlphabetString from '../UtilService/generateRandomAlphabetS
 async function randomlyGenerateUsername(defaultValue: string = null, length = 12): Promise<string> {
   let string = defaultValue || generateRandomAlphabetString();
 
+  const testClient = new Client({ username: string });
+  try {
+    await testClient.validate({ fields: ['username'] });
+  } catch (err) {
+    return randomlyGenerateUsername(null, length);
+  }
+
   if (await Client.findOne({ where: { username: string } })) {
     return randomlyGenerateUsername(null, length);
   }
